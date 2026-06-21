562e7e77d87184a441536442922f66a27c2f1577
ronaldogonzales053-droid <ronaldogonzales053@gmail.com>
2026-06-21 18:24:39 -0500

    Create config.php

---
+++ config.php
@@ -0,0 +1,25 @@
+<?php
+
+$host = getenv('MYSQLHOST');
+$user = getenv('MYSQLUSER');
+$pass = getenv('MYSQLPASSWORD');
+$db   = getenv('MYSQL_DATABASE');
+$port = getenv('MYSQLPORT');
+
+$conn = new mysqli(
+    $host,
+    $user,
+    $pass,
+    $db,
+    $port
+);
+
+if ($conn->connect_error) {
+    die(json_encode([
+        "success" => false,
+        "message" => $conn->connect_error
+    ]));
+}
+
+$conn->set_charset("utf8mb4");
+?>
