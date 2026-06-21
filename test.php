00b69163d8026b608e856a426b493222aa5ca1a7
ronaldogonzales053-droid <ronaldogonzales053@gmail.com>
2026-06-21 18:50:27 -0500

    Create test.php

---
+++ test.php
@@ -0,0 +1,10 @@
+<?php
+
+echo "<pre>";
+
+echo "HOST: " . getenv('MYSQLHOST') . "\n";
+echo "USER: " . getenv('MYSQLUSER') . "\n";
+echo "DB: " . getenv('MYSQLDATABASE') . "\n";
+echo "PORT: " . getenv('MYSQLPORT') . "\n";
+
+?>
