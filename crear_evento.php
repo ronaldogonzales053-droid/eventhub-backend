458757725f67d2fc52895bbd7ea43b871872d251
ronaldogonzales053-droid <ronaldogonzales053@gmail.com>
2026-06-22 00:42:12 -0500

    Create crear_evento.php

---
+++ crear_evento.php
@@ -0,0 +1,50 @@
+<?php
+
+header("Content-Type: application/json");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
+
+require_once "config.php";
+
+$data = json_decode(
+    file_get_contents("php://input"),
+    true
+);
+
+$titulo = $data["titulo"];
+$descripcion = $data["descripcion"];
+$fecha_evento = $data["fecha_evento"];
+$hora_evento = $data["hora_evento"];
+$ubicacion = $data["ubicacion"];
+
+$sql = "INSERT INTO eventos
+(
+titulo,
+descripcion,
+fecha_evento,
+hora_evento,
+ubicacion
+)
+VALUES
+(
+'$titulo',
+'$descripcion',
+'$fecha_evento',
+'$hora_evento',
+'$ubicacion'
+)";
+
+if($conn->query($sql)){
+
+    echo json_encode([
+        "success"=>true
+    ]);
+
+}else{
+
+    echo json_encode([
+        "success"=>false,
+        "message"=>$conn->error
+    ]);
+}
+?>
