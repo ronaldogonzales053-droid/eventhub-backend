217d7321f8e368bf52f2fad224b170424b088cfe
ronaldogonzales053-droid <ronaldogonzales053@gmail.com>
2026-06-22 02:05:22 -0500

    Update obtener_eventos.php

--- obtener_eventos.php
+++ obtener_eventos.php
@@ -2,22 +2,60 @@
 
 header("Content-Type: application/json");
 header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
 
 require_once "config.php";
 
-$sql = "SELECT * FROM eventos";
+$sql = "SELECT *
+        FROM eventos
+        ORDER BY id DESC";
 
 $result = $conn->query($sql);
 
 $eventos = [];
 
-while($row = $result->fetch_assoc()) {
-    $eventos[] = $row;
+if($result){
+
+    while($row =
+        $result->fetch_assoc()){
+
+        $eventos[] = [
+
+            "id" =>
+            $row["id"],
+
+            "titulo" =>
+            $row["titulo"],
+
+            "descripcion" =>
+            $row["descripcion"],
+
+            "fecha_evento" =>
+            $row["fecha_evento"],
+
+            "hora_evento" =>
+            $row["hora_evento"],
+
+            "ubicacion" =>
+            $row["ubicacion"],
+
+            "imagen" =>
+            $row["imagen"] ??
+            "https://picsum.photos/800/400"
+
+        ];
+    }
 }
 
 echo json_encode([
-    "success" => true,
-    "data" => $eventos
-]);
 
+    "success" => true,
+
+    "total" =>
+    count($eventos),
+
+    "data" =>
+    $eventos
+
+]);
 ?>
