c7cbddfa91bd10101004d872e4fd609a917a01c6
ronaldogonzales053-droid <ronaldogonzales053@gmail.com>
2026-06-21 23:53:55 -0500

    Update reservar_evento.php

--- reservar_evento.php
+++ reservar_evento.php
@@ -10,34 +10,35 @@ $data = json_decode(
     true
 );
 
-if(!$data){
+$usuario_id = $data["usuario_id"] ?? 0;
+$evento_id = $data["evento_id"] ?? 0;
+
+if($usuario_id == 0 || $evento_id == 0){
+
     echo json_encode([
-        "success" => false,
-        "message" => "No llegaron datos"
+        "success"=>false,
+        "message"=>"Datos incompletos"
     ]);
     exit;
 }
 
-$usuario_id = $data["usuario_id"] ?? 0;
-$evento_id = $data["evento_id"] ?? 0;
-
 $sql = "INSERT INTO reservas
 (usuario_id, evento_id)
 VALUES
-('$usuario_id', '$evento_id')";
+('$usuario_id','$evento_id')";
 
 if($conn->query($sql)){
 
     echo json_encode([
-        "success" => true,
-        "message" => "Reserva realizada"
+        "success"=>true,
+        "message"=>"Reserva realizada"
     ]);
 
 }else{
 
     echo json_encode([
-        "success" => false,
-        "message" => $conn->error
+        "success"=>false,
+        "message"=>$conn->error
     ]);
 }
 ?>
